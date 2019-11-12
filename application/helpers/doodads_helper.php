3bf3b9011cb3e418712a35efb13f5b9791f27469
Jonathan Rappner <jrappner@gmail.com>
2019-11-12 12:48:29 +0100

    [font] i nyhetsflöded
    lade till support för [font] i nyhetsflöded

--- application/helpers/doodads_helper.php
+++ application/helpers/doodads_helper.php
@@ -210,6 +210,7 @@ function bbcode_parse($text)
 		'~\[hr\]\[\/hr\]~s',
 		'~\[quote\](.*?)\[/quote\]~s',
 		'~\[size=(.*?)\](.*?)\[/size\]~s',
+		'~\[font="(.*?)"\](.*?)\[/font\]~s',
 		'~\[color=(.*?)\](.*?)\[/color\]~s',
 		'~\[url\]((?:ftp|https?)://.*?)\[/url\]~s',
 		'~\[url=(.+?)\](.+?)\[\/url\]~s',
@@ -224,8 +225,9 @@ function bbcode_parse($text)
 		'<i>$1</i>',
 		'<span style="text-decoration:underline;">$1</span>',
 		'<hr>',
-		'<pre>$1</'.'pre>',
+		'<pre>$1</pre>',
 		'<span style="font-size:2rem;">$2</span>', //alt: '<span style="font-size:$1px;">$2</span>'
+		'<span style="font-family:\'$1\'">$2</span>',
 		'<span style="color:$1;">$2</span>',
 		'<a href="$1">$1</a>',
 		'<a href="$1">$2</a>',
