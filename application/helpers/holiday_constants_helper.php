acb91a3fdd4c4b2ae373665c757a7e92e48ce5db
Jonathan Rappner <jrappner@gmail.com>
2019-11-25 17:00:13 +0100

    Update holiday_constants_helper.php

--- application/helpers/holiday_constants_helper.php
+++ application/helpers/holiday_constants_helper.php
@@ -4,11 +4,11 @@
  * Används av både codeigniter och phpbb.
  */
 
-//Första april
+// Första april
 define('APRIL_FOOLS', date('n') == 4 && date('j') == 1);
 
-//Jul 🎅🎁🎄
+// Jul 🎅🎁🎄
 $earliest_first_advent = strtotime('27 november'); //tidigaste möjliga första advent är 27/11 (https://sv.wikipedia.org/wiki/Advent)
 $first_advent = strtotime('sunday', $earliest_first_advent); //hitta första söndagen på, eller efter 27/11
-$eight_jan = mktime(0, 0, 0, 1, 8, (date('Y')+1)); //åttonde januari nästa år (en vecka efter nyårsdagen)
+$eight_jan = mktime(0, 0, 0, 1, 8, (date('Y')+1)); //åttonde januari, nästa år (en vecka efter nyårsdagen)
 define('XMAS', time() >= $first_advent && time() < $eight_jan);
