8041284af68602d55de0d6ab2cd69aa7a8daea06
Jonathan Rappner <jrappner@gmail.com>
2020-01-20 21:43:16 +0100

    Jul-logik-fix

--- application/helpers/holiday_constants_helper.php
+++ application/helpers/holiday_constants_helper.php
@@ -10,5 +10,8 @@ define('APRIL_FOOLS', date('n') == 4 && date('j') == 1);
 // Jul 🎅🎁🎄
 $earliest_first_advent = strtotime('27 november'); //tidigaste möjliga första advent är 27/11 (https://sv.wikipedia.org/wiki/Advent)
 $first_advent = strtotime('sunday', $earliest_first_advent); //hitta första söndagen på, eller efter 27/11
-$eight_jan = mktime(0, 0, 0, 1, 8, (date('Y')+1)); //åttonde januari, nästa år (en vecka efter nyårsdagen)
-define('XMAS', time() >= $first_advent && time() < $eight_jan);
+$sixth_jan = strtotime('6 january'); //åttonde januari (en vecka efter nyårsdagen)
+define('XMAS', time() >= $first_advent || time() < $sixth_jan); //efter första advent eller före 6:e jan
+
+
+///////////////lista ut när treddondagen är
