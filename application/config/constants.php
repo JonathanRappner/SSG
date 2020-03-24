595955bfc57044de66b78ab1fd3fb3316987de76
Jonathan Rappner <jrappner@gmail.com>
2020-03-24 15:44:46 +0100

    1.1.1

--- application/config/constants.php
+++ application/config/constants.php
@@ -85,5 +85,5 @@ defined('EXIT__AUTO_MIN')      OR define('EXIT__AUTO_MIN', 9); // lowest automat
 defined('EXIT__AUTO_MAX')      OR define('EXIT__AUTO_MAX', 125); // highest automatically-assigned error code
 
 //--Version--
-define('SSG_VERSION', '1.1.0');
-define('SSG_BUILD_DATE', '2019-11-28');
+define('SSG_VERSION', '1.1.1');
+define('SSG_BUILD_DATE', '2020-03-24');
