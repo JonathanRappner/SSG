111b09299fd87fcc4d337fd0c923d5475b07b989
Jonathan Rappner <jrappner@gmail.com>
2020-04-01 22:53:03 +0200

    Tweaks

--- application/helpers/holiday_constants_helper.php
+++ application/helpers/holiday_constants_helper.php
@@ -8,19 +8,14 @@
 define('APRIL_FOOLS', date('n') == 4 && date('j') == 1);
 
 // Jul 🎅🎁🎄
-$earliest_first_advent = strtotime('27 november'); //tidigaste möjliga första advent är 27/11 (https://sv.wikipedia.org/wiki/Advent)
-$first_advent = strtotime('sunday', $earliest_first_advent); //hitta första söndagen på, eller efter 27/11
-$sixth_jan = strtotime('6 january'); //åttonde januari (en vecka efter nyårsdagen)
-define('XMAS', time() >= $first_advent || time() < $sixth_jan); //efter första advent eller före 6:e jan
+$earliest_first_advent = strtotime('27 november'); // tidigaste möjliga första advent är 27/11 (https://sv.wikipedia.org/wiki/Advent)
+$first_advent = strtotime('sunday', $earliest_first_advent); // hitta första söndagen på, eller efter 27/11
+$sixth_jan = strtotime('6 january'); // åttonde januari (en vecka efter nyårsdagen)
+define('XMAS', time() >= $first_advent || time() < $sixth_jan); // efter första advent eller före 6:e jan
 ///////////////lista ut när treddondagen är
 
 // Första torsdagen i mars
 // Hitta första mars, sedan hitta tidigaste torsdagen
-$first_march = mktime(
-	0, 0, 0, // h, m, s
-	3, // mars
-	1, // första
-	date('Y') // detta året
-);
+$first_march = strtotime('1 march');
 $first_thursday = date('Y-m-d', strtotime('thursday', $first_march));
 define('CAKE', date('Y-m-d') == $first_thursday); // är årets första torsdag i mars, idag?
