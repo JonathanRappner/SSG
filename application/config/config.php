dabd7a3d6ab1f6612e089b34facf06711e9d973e
Jonathan Rappner <jrappner@gmail.com>
2020-05-08 15:57:27 +0200

    Inloggnings-session = 1 vecka

--- application/config/config.php
+++ application/config/config.php
@@ -381,7 +381,7 @@ $config['encryption_key'] = '';
 */
 $config['sess_driver'] = 'database';
 $config['sess_cookie_name'] = 'ssg_session';
-$config['sess_expiration'] = (60*60)*48; //48h
+$config['sess_expiration'] = 60*60*24*7; // en vecka
 $config['sess_save_path'] = 'ssg_sessions';
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
