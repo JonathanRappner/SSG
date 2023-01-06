30ee6ebff71d344069d83223a3d95e0bc4a63860
Jonathan Rappner <jrappner@gmail.com>
2023-01-06 12:59:03 +0100

    2023

--- ssg signup stats.sql
+++ ssg signup stats.sql
@@ -5,25 +5,29 @@ SELECT
 FROM ssg_members AS m
 JOIN ssg_signups AS s
 	ON m.id = s.member_id
+JOIN ssg_events AS e
+	ON s.event_id = e.id
 WHERE
-	s.signed_datetime BETWEEN '2021-01-01' AND '2021-12-31'
+	s.signed_datetime BETWEEN '2022-01-01' AND '2022-12-31'
 	AND m.is_active = 1
 	AND s.attendance <= 3
+	AND e.obligatory = 1
 GROUP BY m.id
 ORDER BY signups_count DESC
 
 --Antal timmar (genomsnitt) i förväg som anmälan gjorts
 SELECT
 	m.name,
-    ROUND(AVG(DATEDIFF(e.start_datetime, s.signed_datetime)), 1) AS signed_time
+	ROUND(AVG(DATEDIFF(e.start_datetime, s.signed_datetime)), 1) AS signed_time
 FROM ssg_members AS m
 JOIN ssg_signups AS s
 	ON m.id = s.member_id
 JOIN ssg_events AS e
 	ON s.event_id = e.id
 WHERE
-	s.signed_datetime BETWEEN '2021-01-01' AND '2021-12-31'
+	s.signed_datetime BETWEEN '2022-01-01' AND '2022-12-31'
 	AND m.is_active = 1
-    AND s.attendance <= 3
+	AND s.attendance <= 3
+	AND e.obligatory = 1
 GROUP BY m.id
 ORDER BY signed_time DESC;
