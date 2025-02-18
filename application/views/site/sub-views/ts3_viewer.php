3b4cb2ade797032a545e1e78aecd917fd98d73bc
Jonathan Rappner <jrappner@gmail.com>
2025-02-18 17:00:39 +0100

    ts3viewer ny kod

--- application/views/site/sub-views/ts3_viewer.php
+++ application/views/site/sub-views/ts3_viewer.php
@@ -3,6 +3,7 @@
  * Teamspeak 3 viewer
 */
 defined('BASEPATH') OR exit('No direct script access allowed');
+define('TS3VIEWER_ID', '1130234')
 ?><div class="card ts3_viewer border-0 shadow-sm">
 
 	<div class="card-header bg-dark text-white">
@@ -14,13 +15,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 	<div class="card-body p-2">
 
-		<div id="ts3viewer_1128123"> </div>
+		<div id="ts3viewer_<?=TS3VIEWER_ID?>"> </div>
 
 		<script type="text/javascript" src="https://static.tsviewer.com/short_expire/js/ts3viewer_loader.js"></script>
 		<script type="text/javascript">
 			var ts3v_url_1 =
-				"https://www.tsviewer.com/ts3viewer.php?ID=1128123&text=000000&text_size=12&text_family=1&js=1&text_s_weight=bold&text_s_style=normal&text_s_variant=normal&text_s_decoration=none&text_s_color_h=525284&text_s_weight_h=bold&text_s_style_h=normal&text_s_variant_h=normal&text_s_decoration_h=underline&text_i_weight=normal&text_i_style=normal&text_i_variant=normal&text_i_decoration=none&text_i_color_h=525284&text_i_weight_h=normal&text_i_style_h=normal&text_i_variant_h=normal&text_i_decoration_h=underline&text_c_weight=normal&text_c_style=normal&text_c_variant=normal&text_c_decoration=none&text_c_color_h=525284&text_c_weight_h=normal&text_c_style_h=normal&text_c_variant_h=normal&text_c_decoration_h=underline&text_u_weight=bold&text_u_style=normal&text_u_variant=normal&text_u_decoration=none&text_u_color_h=525284&text_u_weight_h=bold&text_u_style_h=normal&text_u_variant_h=normal&text_u_decoration_h=none";
-			ts3v_display.init(ts3v_url_1, 1128123, 100);
+				"https://www.tsviewer.com/ts3viewer.php?ID=<?=TS3VIEWER_ID?>&text=000000&text_size=12&text_family=1&js=1&text_s_weight=bold&text_s_style=normal&text_s_variant=normal&text_s_decoration=none&text_s_color_h=525284&text_s_weight_h=bold&text_s_style_h=normal&text_s_variant_h=normal&text_s_decoration_h=underline&text_i_weight=normal&text_i_style=normal&text_i_variant=normal&text_i_decoration=none&text_i_color_h=525284&text_i_weight_h=normal&text_i_style_h=normal&text_i_variant_h=normal&text_i_decoration_h=underline&text_c_weight=normal&text_c_style=normal&text_c_variant=normal&text_c_decoration=none&text_c_color_h=525284&text_c_weight_h=normal&text_c_style_h=normal&text_c_variant_h=normal&text_c_decoration_h=underline&text_u_weight=bold&text_u_style=normal&text_u_variant=normal&text_u_decoration=none&text_u_color_h=525284&text_u_weight_h=bold&text_u_style_h=normal&text_u_variant_h=normal&text_u_decoration_h=none";
+			ts3v_display.init(ts3v_url_1, <?=TS3VIEWER_ID?>, 100);
 		</script>
 
 	</div>
