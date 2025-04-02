11711726c123ddde8481745aceccd8b2c9fea3bf
Jonathan Rappner <jrappner@gmail.com>
2025-04-02 17:11:01 +0200

    new member tog bort instruktioner tillfälligt

--- application/views/site/sub-views/new_member_welcome.php
+++ application/views/site/sub-views/new_member_welcome.php
@@ -9,14 +9,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<div class="card mr-4 bg-white border-0 shadow-sm">
 
 		<h1>Välkommen till SSG</h1>
-		<h1>Welcome to SSG</h1>
-		<h3>(english below)</h3>
-
-		<p>Vad roligt att du är här! Vi ser gärna att du registrerar dig på hemsidan så att du får mer tillgång till vad som händer och du får även tillgång till forumet.</p>
-		<p>Hi! Nice to see you here. We would love it if you registered an account for this page. This will give you access to what is going on, and also give you access to the forums.</p>
-		<p>If you want to contact us in anyway, please register and <a href="<?=base_url('forum/ucp.php?i=pm&mode=compose&u=65')?>">send a PM to Kalle</a>.</p>
-		<p>Tack!</p>
-		<img src="https://www.ssg-clan.se/images/ssg_2021_frontpage.jpg" alt="Welcome">
 
 		<h2>Vilka är SSG?</h2>
 		<p>SSG grundades 2009 och är en stabil och väletablerad ArmA-klan med båda fötterna på jorden. Vi är inriktade på taktisk realism med ett stenhårt gameface under operationerna men med en välkomnande och avslappnad stämning utanför spel.</p>
@@ -26,13 +18,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		<p>Vi använder ett hierarkiskt system både i spelet och utanför. Det är dock inte samma personer som styr i spelet och utanför - Plutonledning och Klanledning är särskilda ansvarsområden. Vi har ansvar uppdelat på ett flertal grupper som jobbar mot att förbättra spelupplevelsen och driva klanen framåt. Det är dock fritt för varje medlem att komma med förbättringsförslag till gagn för klanen både i spelsätt, utrustning och addons - vi uppmuntrar engagemang.</p>
 		<p>Vi brukar övervägande använda svensk utrustning och material men är inte ovana till att även nyttja material från andra länder om vi bedömer att detta är positivt för spelupplevelsen.</p>
 		
-		<div class="mt-4">
-			<p class="font-weight-bold">Vill du spela med oss?</p>
-			<p>[ <a href="<?=base_url('forum/viewtopic.php?f=4&t=1789')?>">Klicka här</a> ]</p>
-			<p class="font-weight-bold">för att komma till ansökan.</p>
-			<p>OBS! Du måste ha en fungerande epost-adress.</p>
-		</div>
-
 	</div>
 
 </div>
