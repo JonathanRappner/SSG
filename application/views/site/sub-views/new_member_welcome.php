ac715ce8491689e986db716856587d639b95eef9
Jonathan Rappner <jrappner@gmail.com>
2025-04-10 17:17:21 +0200

    "kreativa småändringar"

--- application/views/site/sub-views/new_member_welcome.php
+++ application/views/site/sub-views/new_member_welcome.php
@@ -3,6 +3,9 @@
  * Teamspeak 3 viewer
 */
 defined('BASEPATH') OR exit('No direct script access allowed');
+define('SVG_DISCORD', '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" width="32" height="32">
+					<path fill="white" d="M524.5 69.8a1.5 1.5 0 0 0 -.8-.7A485.1 485.1 0 0 0 404.1 32a1.8 1.8 0 0 0 -1.9 .9 337.5 337.5 0 0 0 -14.9 30.6 447.8 447.8 0 0 0 -134.4 0 309.5 309.5 0 0 0 -15.1-30.6 1.9 1.9 0 0 0 -1.9-.9A483.7 483.7 0 0 0 116.1 69.1a1.7 1.7 0 0 0 -.8 .7C39.1 183.7 18.2 294.7 28.4 404.4a2 2 0 0 0 .8 1.4A487.7 487.7 0 0 0 176 479.9a1.9 1.9 0 0 0 2.1-.7A348.2 348.2 0 0 0 208.1 430.4a1.9 1.9 0 0 0 -1-2.6 321.2 321.2 0 0 1 -45.9-21.9 1.9 1.9 0 0 1 -.2-3.1c3.1-2.3 6.2-4.7 9.1-7.1a1.8 1.8 0 0 1 1.9-.3c96.2 43.9 200.4 43.9 295.5 0a1.8 1.8 0 0 1 1.9 .2c2.9 2.4 6 4.9 9.1 7.2a1.9 1.9 0 0 1 -.2 3.1 301.4 301.4 0 0 1 -45.9 21.8 1.9 1.9 0 0 0 -1 2.6 391.1 391.1 0 0 0 30 48.8 1.9 1.9 0 0 0 2.1 .7A486 486 0 0 0 610.7 405.7a1.9 1.9 0 0 0 .8-1.4C623.7 277.6 590.9 167.5 524.5 69.8zM222.5 337.6c-29 0-52.8-26.6-52.8-59.2S193.1 219.1 222.5 219.1c29.7 0 53.3 26.8 52.8 59.2C275.3 311 251.9 337.6 222.5 337.6zm195.4 0c-29 0-52.8-26.6-52.8-59.2S388.4 219.1 417.9 219.1c29.7 0 53.3 26.8 52.8 59.2C470.7 311 447.5 337.6 417.9 337.6z"/>
+				</svg>')
 ?><link rel="stylesheet" href="<?=base_url('css/site/new_member_welcome.css')?>">
 <div id="new_member_welcome" class="row mb-4 p-0 pr-1">
 
@@ -19,24 +22,29 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		<p>
 			Varje söndag mellan klockan 18-22 spelas en operation som är byggt av vår uppdragsgrupp där briefing, intel och annat av intresse postas i forumet så att alla har en inblick i vilket motstånd vi kommer att möta.
 		</p>
+
+		<p class="mt-4">Vill du spela med oss?</p>
+
 		<p>
 			<a class="btn btn-discord mt-4" href="https://discord.gg/6d5GYyBrQt">
-				Vill du spela med oss?
-				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" width="32" height="32">
-					<path fill="white" d="M524.5 69.8a1.5 1.5 0 0 0 -.8-.7A485.1 485.1 0 0 0 404.1 32a1.8 1.8 0 0 0 -1.9 .9 337.5 337.5 0 0 0 -14.9 30.6 447.8 447.8 0 0 0 -134.4 0 309.5 309.5 0 0 0 -15.1-30.6 1.9 1.9 0 0 0 -1.9-.9A483.7 483.7 0 0 0 116.1 69.1a1.7 1.7 0 0 0 -.8 .7C39.1 183.7 18.2 294.7 28.4 404.4a2 2 0 0 0 .8 1.4A487.7 487.7 0 0 0 176 479.9a1.9 1.9 0 0 0 2.1-.7A348.2 348.2 0 0 0 208.1 430.4a1.9 1.9 0 0 0 -1-2.6 321.2 321.2 0 0 1 -45.9-21.9 1.9 1.9 0 0 1 -.2-3.1c3.1-2.3 6.2-4.7 9.1-7.1a1.8 1.8 0 0 1 1.9-.3c96.2 43.9 200.4 43.9 295.5 0a1.8 1.8 0 0 1 1.9 .2c2.9 2.4 6 4.9 9.1 7.2a1.9 1.9 0 0 1 -.2 3.1 301.4 301.4 0 0 1 -45.9 21.8 1.9 1.9 0 0 0 -1 2.6 391.1 391.1 0 0 0 30 48.8 1.9 1.9 0 0 0 2.1 .7A486 486 0 0 0 610.7 405.7a1.9 1.9 0 0 0 .8-1.4C623.7 277.6 590.9 167.5 524.5 69.8zM222.5 337.6c-29 0-52.8-26.6-52.8-59.2S193.1 219.1 222.5 219.1c29.7 0 53.3 26.8 52.8 59.2C275.3 311 251.9 337.6 222.5 337.6zm195.4 0c-29 0-52.8-26.6-52.8-59.2S388.4 219.1 417.9 219.1c29.7 0 53.3 26.8 52.8 59.2C470.7 311 447.5 337.6 417.9 337.6z"/>
-				</svg>
+				Ansök nu!
+				<?=SVG_DISCORD?>
 			</a>
 		</p>
 
 		<hr>
 
-		<!-- <h2>Frågor</h2>
+		<h2>Frågor</h2>
 
 		<p>
-			Om du har några funderingar eller frågor angående hur vi spelar, om att bli medlem eller om något annat är du välkommen att skriva här i chatten. Vi kommer försöka svara så snart som möjligt. 
+			Om du har några funderingar eller frågor angående hur vi spelar, om att bli medlem eller om något annat är du välkommen att skriva ställ-en-fråga-kanalen i vår Discord. Vi kommer försöka svara så snart som möjligt.
+			<a class="btn btn-discord mt-4" href="https://discord.com/channels/520347795413139474/842037029474533396">
+				Fråga här!
+				<?=SVG_DISCORD?>
+			</a>
 		</p>
 
-		<hr> -->
+		<hr>
 
 		<h2>JointOps</h2>
 
@@ -48,9 +56,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		<p>
 		<a class="btn btn-discord mt-4" href="https://discord.gg/Xa4zZSWS">
 				Questions and JointOps
-				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" width="32" height="32">
-					<path fill="white" d="M524.5 69.8a1.5 1.5 0 0 0 -.8-.7A485.1 485.1 0 0 0 404.1 32a1.8 1.8 0 0 0 -1.9 .9 337.5 337.5 0 0 0 -14.9 30.6 447.8 447.8 0 0 0 -134.4 0 309.5 309.5 0 0 0 -15.1-30.6 1.9 1.9 0 0 0 -1.9-.9A483.7 483.7 0 0 0 116.1 69.1a1.7 1.7 0 0 0 -.8 .7C39.1 183.7 18.2 294.7 28.4 404.4a2 2 0 0 0 .8 1.4A487.7 487.7 0 0 0 176 479.9a1.9 1.9 0 0 0 2.1-.7A348.2 348.2 0 0 0 208.1 430.4a1.9 1.9 0 0 0 -1-2.6 321.2 321.2 0 0 1 -45.9-21.9 1.9 1.9 0 0 1 -.2-3.1c3.1-2.3 6.2-4.7 9.1-7.1a1.8 1.8 0 0 1 1.9-.3c96.2 43.9 200.4 43.9 295.5 0a1.8 1.8 0 0 1 1.9 .2c2.9 2.4 6 4.9 9.1 7.2a1.9 1.9 0 0 1 -.2 3.1 301.4 301.4 0 0 1 -45.9 21.8 1.9 1.9 0 0 0 -1 2.6 391.1 391.1 0 0 0 30 48.8 1.9 1.9 0 0 0 2.1 .7A486 486 0 0 0 610.7 405.7a1.9 1.9 0 0 0 .8-1.4C623.7 277.6 590.9 167.5 524.5 69.8zM222.5 337.6c-29 0-52.8-26.6-52.8-59.2S193.1 219.1 222.5 219.1c29.7 0 53.3 26.8 52.8 59.2C275.3 311 251.9 337.6 222.5 337.6zm195.4 0c-29 0-52.8-26.6-52.8-59.2S388.4 219.1 417.9 219.1c29.7 0 53.3 26.8 52.8 59.2C470.7 311 447.5 337.6 417.9 337.6z"/>
-				</svg>
+				<?=SVG_DISCORD?>
 			</a>
 		</p>
 
